3287250f333f573f9b5cf61d4dec02cd44e4bc5a
Keryn Knight <keryn@kerynknight.com>
2021-08-31 17:05:28 +0100

    Attempt to ensure that the technical debug pages for 500s force a full page reload. This allows a template error showing the classic yellow error page to be rectified, and have the correct stylesheets etc loaded in when it happens.
    See 2edc182250c61813d5bc23c0fc3919cc76434c6a for client side implementation.

--- livereloadish/middleware.py
+++ livereloadish/middleware.py
@@ -22,8 +22,22 @@ class NamedUrlconf(namedtuple("NamedUrl", "included_patterns")):
 class LivereloadishMiddleware:
     prefix = "livereloadish"
     content_types = ("text/html", "application/xhtml+xml")
-    insert_js_before = "</body>"
-    insert_js_content = f'<script data-livereloadish-id="{{uuid}}" type="text/javascript" data-livereloadish-url="/{prefix}/watch/?livereloadish={{uuid}}" src="/{{prefix}}/watcher/livereloadish.js" defer async data-turbolinks="false" data-turbolinks-eval="false"></script></body>'
+    # SSE insertion. Happens at the end of the </head> but don't worry it's marked
+    # as async & defer, so it'll not block page load.
+    insert_js_before = "</head>"
+    insert_js_content = f'<script data-livereloadish-id="{{uuid}}" type="text/javascript" data-livereloadish-url="/{prefix}/watch/?livereloadish={{uuid}}" src="/{{prefix}}/watcher/livereloadish.js" defer async data-turbolinks="false" data-turbolinks-eval="false"></script>\n</head>'
+    # When an error page (technical_404, technical_500) is shown, we want to
+    # force a full page reload if they connect to the SSE, so that styles etc get
+    # re-applied where they might not otherwise (eg: if I end up doing a udomdiff
+    # on the body)
+    insert_meta_before = "</head>"
+    insert_meta_content = (
+        '<meta name="livereloadish-css-strategy" content="reload">\n'
+        '<meta name="livereloadish-image-strategy" content="reload">\n'
+        '<meta name="livereloadish-js-strategy" content="reload">\n'
+        '<meta name="livereloadish-page-strategy" content="reload">\n'
+        "</head>"
+    )
 
     def __init__(self, get_response: Any) -> None:
         if not settings.DEBUG:
@@ -45,24 +59,40 @@ class LivereloadishMiddleware:
         # it's been rock solid for my usage for 10 years, can't be totally wrong.
         content_encoding = response.headers.get("Content-Encoding", "")
         content_type = response.headers.get("Content-Type", "").partition(";")[0]
-        # Additionally I don't want to load the SSE connection for 401/403/404 etc
-        # because those cannot be rectified by a CSS/JS/HTML change so the auto-reloader
-        # would kick in for the Python/Django change.
-        # Note that it is still turned on for 500 errors (ie: the technical debug page)
-        # because those may stem from TemplateSyntaxError, which is resolvable.
         if (
             getattr(response, "streaming", False)
             or "gzip" in content_encoding
             or content_type not in self.content_types
-            or (400 < response.status_code < 500)
         ):
             logger.debug(
-                "Livereloadish not being mounted for path %s with HTTP status=%s",
+                "Livereloadish not being mounted for path %s",
                 request.path,
                 response.status_code,
             )
             return response
         content = response.content.decode(response.charset)
+        # I don't want to load the SSE connection for 401/403/404 etc
+        # because those cannot be rectified by a CSS/JS/HTML change so the auto-reloader
+        # would kick in for the Python/Django change.
+        # Note that it is still turned on for 500 errors (ie: the technical debug page)
+        # because those may stem from TemplateSyntaxError, which is resolvable.
+        # But we make sure we're doing a full page reload if the change came from
+        # a template (or anything else, though they can't fix it), so that any
+        # debug styles are flushed away and correct stylesheets etc are loaded in.
+        if response.status_code >= 500 and self.insert_meta_before in content:
+            logger.debug(
+                "Livereloadish is telling the error page to do full page reloads for %s",
+                request.path,
+            )
+            response.content = content.replace(
+                self.insert_meta_before,
+                self.insert_meta_content,
+            )
+            if "Content-Length" in response.headers:
+                response["Content-Length"] = len(response.content)
+            # TODO: avoid decoding it again here for the next if branch? IDK.
+            content = response.content.decode(response.charset)
+
         if self.insert_js_before in content:
             logger.debug("Livereloadish is being mounted for path %s", request.path)
             response.content = content.replace(
